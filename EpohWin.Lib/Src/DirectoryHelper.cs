e0c3ba21927acd475255b35094b06387d8c54ece
sxydh <sxydhgg@gmail.com>
2024-10-21 00:35:09 +0800

    [UPDATE]

---
+++ EpohWin.Lib/Src/DirectoryHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace EpohWin.Lib
+{
+    public class DirectoryHelper
+    {
+        public static string Current()
+        {
+            return Directory.GetCurrentDirectory();
+        }
+
+        public static string GetMethodIdMap()
+        {
+            var ret = "";
+            ret += $"lib/dir-current={typeof(DirectoryHelper).FullName}#Current";
+            return ret;
+        }
+    }
+}
