cc65bcf77a05edc508f96225575bfe39ec2779d1
sxydh <sxydhgg@gmail.com>
2024-10-20 17:59:07 +0800

    [UPDATE]

---
+++ EpohWin.Test/AppTest/ProgramTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EpohWin.Test.AppTest
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        [TestMethod]
+        public void Test()
+        {
+        }
+    }
+}
