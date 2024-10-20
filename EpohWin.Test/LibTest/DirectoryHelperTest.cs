253f74f17ee5cdcb1012978cba9201e7e38568e7
sxydh <sxydhgg@gmail.com>
2024-10-21 00:36:56 +0800

    [UPDATE]

---
+++ EpohWin.Test/LibTest/DirectoryHelperTest.cs
@@ -0,0 +1,16 @@
+﻿using EpohWin.Lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EpohWin.Test.LibTest
+{
+    [TestClass]
+    public class DirectoryHelperTest
+    {
+        [TestMethod]
+        public void TestCurrent()
+        {
+            var current = DirectoryHelper.Current();
+            Assert.IsNotNull(current);
+        }
+    }
+}
