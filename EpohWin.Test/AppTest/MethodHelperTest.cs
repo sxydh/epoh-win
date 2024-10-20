18477046a39ccb999a7425c385f2348645a49772
sxydh <sxydhgg@gmail.com>
2024-10-20 17:58:20 +0800

    [UPDATE]

---
+++ EpohWin.Test/AppTest/MethodHelperTest.cs
@@ -0,0 +1,15 @@
+﻿using EpohWin.App;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EpohWin.Test.AppTest
+{
+    [TestClass]
+    public class MethodHelperTest
+    {
+        [TestMethod]
+        public void TestGetMethodId()
+        {
+            MethodHelper.GetMethodId("say-hello", out _);
+        }
+    }
+}
