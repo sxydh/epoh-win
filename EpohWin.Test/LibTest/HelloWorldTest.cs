058826c38dafec4bf9b853025f7b298cc5a11cc7
sxydh <sxydhgg@gmail.com>
2024-10-20 18:07:10 +0800

    [UPDATE]

---
+++ EpohWin.Test/LibTest/HelloWorldTest.cs
@@ -0,0 +1,16 @@
+﻿using EpohWin.Lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EpohWin.Test.LibTest
+{
+    [TestClass]
+    public class HelloWorldTest
+    {
+        [TestMethod]
+        public void TestSayHello()
+        {
+            var ret = HelloWorld.SayHello("Jack");
+            Assert.IsTrue(ret == "Hello Jack");
+        }
+    }
+}
---
+++ EpohWin.Test/LibTest/SQLiteHelperTest.cs
@@ -0,0 +1,24 @@
+﻿using EpohWin.Lib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+
+namespace EpohWin.Test.LibTest
+{
+    [TestClass]
+    public class SQLiteHelperTest
+    {
+        [TestMethod]
+        public void TestSelect()
+        {
+            var req = new SQLiteReq
+            {
+                Sql = "select #p1 as i, #p2 as s, #p3 as n",
+                Args = new object[] { 1, "1", null },
+                File = $"{typeof(SQLiteHelperTest).FullName}.sqlite"
+            };
+            var reqStr = JsonConvert.SerializeObject(req);
+            var ret = SQLiteHelper.Execute(reqStr);
+            Assert.IsNotNull(ret);
+        }
+    }
+}
